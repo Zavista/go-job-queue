86f306be206aac4e22c9001007d12d38b2d42ee1
Zavista <84294353+Zavista@users.noreply.github.com>
2026-04-01 14:13:43 -0400

    Update main to test code

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/zavista/go-job-queue/internal/jobs"
+	"github.com/zavista/go-job-queue/internal/processors"
+)
 
 func main() {
-	fmt.Println("Hello Word!")
+	manager := jobs.NewManager(10)
+	workerPool := jobs.NewWorkerPool(2, manager)
+
+	workerPool.Start()
+
+	job := manager.AddJob(processors.EmailJob{
+		To:      "test@example.com",
+		Subject: "welcome",
+	})
+
+	fmt.Println("added job:", job.ID, job.Name, job.Status)
+
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("final job state:")
+	fmt.Println("id:", job.ID)
+	fmt.Println("name:", job.Name)
+	fmt.Println("status:", job.Status)
+	fmt.Println("attempts:", job.Attempts)
+	fmt.Println("result:", job.Result)
+
+	if job.Err != nil {
+		fmt.Println("error:", job.Err)
+	}
+
 }
